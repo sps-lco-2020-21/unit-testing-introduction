c0186b0c6610a43bd9169a5083aadf9169a498c3
Chris Harrison <cah@stpaulsschool.org.uk>
2021-01-11 12:36:25 +0000

    Prime Serviexample added

---
+++ PrimeService/PrimeService.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PrimeService.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PrimeService.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5fd8c7e5-96ff-463e-a70a-9466ab1a367c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# unit-testing-introduction
+# Unit Testing Introduction
+
+The `PrimeService` example is included in this repo. This will open in Visual Studio. 
+
+### VS.Code
+
+The .NET Core libraries used in VS.Code are slightly different, but there is [this tutorial](https://docs.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-dotnet-test) on the Microsoft website.  
